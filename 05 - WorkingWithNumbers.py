b6d24c803b2f36746917686b5d96e07eeb82c96b
lukaszgo <lukaszgo@protonmail.com>
2019-09-07 02:30:40 +0200

    After part 5

---
+++ 05 - WorkingWithNumbers.py
@@ -0,0 +1,13 @@
+from math import  *
+
+my_num = -5
+print("Wartość bezwzględna: "+str(abs(my_num)))
+print("Potęga: "+str(pow(2,34)))
+print("Największa wartość: "+str(max(2,34)))
+print("Najmniejsza wartość: "+str(min(2,34)))
+print("Zaokrągli 3,7: "+str(round(3.7)))
+print("Zaokrągli mniej 3,7: "+str(floor(3.7)))
+print("Zaokrągli więcej 3,7: "+str(ceil(3.7)))
+print("Pierwiastek: "+str(sqrt(4)))
+#reszta z dzielenia
+reszta=10%3
