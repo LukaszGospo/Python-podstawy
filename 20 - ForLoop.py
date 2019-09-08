713a4aeaeefb39a7269ba5400a9671ec01e01bca
user <user@DESKTOP-10G8CCG>
2019-09-09 00:58:11 +0200

    After 20

---
+++ 20 - ForLoop.py
@@ -0,0 +1,12 @@
+for letter in "Gospo":
+    print(letter)
+
+friends = ["Kevin", "Karen", "Jim"]
+for friend in friends:
+    print(friend)
+
+for i in range(3, 10):
+    print(i)
+
+for i in range(len(friends)):
+    print(friends[i])
