49f591072850d14e6a9e83692b919d3e7319b561
user <user@DESKTOP-10G8CCG>
2019-09-10 00:35:36 +0200

    After 31

---
+++ 31 - ObjectFunctions.py
@@ -0,0 +1,20 @@
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
+
+
+# from Student import Student
+
+student1 = Student("Jim", "MBA", 3.1, False)
+
+print(student1.on_honor_roll())
